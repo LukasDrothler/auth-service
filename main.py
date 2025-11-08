118df07d93ad07b717fdf13b4e8aa60689149f32
ImpulZzZ <lukas.drothler@web.de>
2025-11-08 16:14:35 +0100

    refactor main.py to configure logging and FastAPI settings based on environment

--- main.py
+++ main.py
@@ -6,29 +6,49 @@ from src.dependencies import *
 import src.routers.token as token
 import src.routers.user as user
 
+import os
 import logging
 import uvicorn
 
 from dotenv import load_dotenv
 
-# Configure logging
+load_dotenv()
+is_dev = False
+
+try:
+    if os.getenv('CURRENT_ENV') == 'development':
+        is_dev = True
+except KeyError:
+    pass
+
+if is_dev:
+    log_level = logging.INFO
+    hot_reload = True
+    docs_url = "/docs"
+    redoc_url = "/redoc"
+else:
+    log_level = logging.WARNING
+    hot_reload = False
+    docs_url = None
+    redoc_url = None
+    
+
+app = FastAPI(
+        title="Auth Service API",
+        description="API for Auth Service",
+        docs_url=docs_url,
+        redoc_url=redoc_url,
+    )
+
 logging.basicConfig(
-    level=logging.INFO,
+    level=log_level,
     format='%(asctime)s %(levelname)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S'
 )
 
-load_dotenv()
 setup_dependencies(
     access_token_expire_minutes=30,
     refresh_token_expire_days=30,
-)
-
-app = FastAPI(
-    title="Auth Service API",
-    description="API for Auth Service",
-    # docs_url=None,
-    # redoc_url=None,
 )
 
 app.add_middleware(
@@ -44,4 +64,4 @@ app.include_router(user.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run("main:app", host=os.getenv("FASTAPI_HOST", "0.0.0.0"), port=int(os.getenv("FASTAPI_PORT", 8000)), reload=hot_reload)
